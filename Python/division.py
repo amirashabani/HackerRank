cae8338b8b2e517c97b1718582b70731f83d207f
amirashabani <amirshabani011@gmail.com>
2021-08-19 16:44:20 +0430

    Division

---
+++ Python/division.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-division/problem
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a / b)
+
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Number of solutions in each category:
 
-Python: 3
+Python: 4
