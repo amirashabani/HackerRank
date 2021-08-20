026be579ee49be2a77f8e306a11ea0b184b94d6f
amirashabani <amirshabani011@gmail.com>
2021-08-21 01:37:29 +0430

    Tuples

---
+++ Python/tuples.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+n = int(input())
+
+t = tuple(map(int, input().split()))
+print(hash(t))
+
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Number of solutions in each category:
 
-Python: 12
+Python: 13
