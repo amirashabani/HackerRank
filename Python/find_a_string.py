076215057a663644d7f31ef37e50fe6bc2b9323e
amirashabani <amirshabani011@gmail.com>
2021-08-21 17:50:20 +0430

    Find a string

---
+++ Python/find_a_string.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(string, sub_string):
+    count = 0
+    strlen = len(string)
+    sublen = len(sub_string)
+
+    for i in range(strlen-sublen+1):
+        if string[i:i+sublen] == sub_string:
+            count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
+
