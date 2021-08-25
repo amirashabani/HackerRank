f6f09ab35c3d04319333a36aac8121d70e795691
amirashabani <amirshabani011@gmail.com>
2021-08-25 12:42:11 +0430

    Exceptions

---
+++ Python/exceptions.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/exceptions/problem
+
+n = int(input())
+
+for i in range(n):
+    a, b = input().split()
+    try:
+        print(int(a) // int(b))
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+    except ValueError as e:
+        print("Error Code:", e)
+
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Number of solutions in each category:
 
-Python: 35
+Python: 36
