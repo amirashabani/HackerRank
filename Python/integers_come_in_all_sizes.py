2f272b839cfd93c7e8f462b0f0faa282a42af9a2
amirashabani <amirshabani011@gmail.com>
2021-08-27 16:00:07 +0430

    Integers Come In All Sizes

---
+++ Python/integers_come_in_all_sizes.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+a, b, c, d = [int(input()) for _ in range(4)]
+
+result = (a ** b) + (c ** d)
+print(result)
+
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Number of solutions in each category:
 
-Python: 38
+Python: 39
