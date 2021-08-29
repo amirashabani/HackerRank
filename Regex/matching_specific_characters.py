69ad4b2c352c51e9d6c33e8f7156b3b6bea244fc
amirashabani <amirshabani011@gmail.com>
2021-08-30 03:48:09 +0430

    Matching Specific Characters

---
+++ Regex/matching_specific_characters.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/matching-specific-characters/problem
+
+Regex_Pattern = r"^[123][120][xs0][30Aa][xsu][\.,]$"
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
+
